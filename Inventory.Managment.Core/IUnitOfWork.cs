e40690a14f3aa2968ef0cea4e278cf1d2a2aed33
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2025-03-10 05:18:09 +0200

    fixed typo

--- Inventory.Managment.Core/IUnitOfWork.cs
+++ Inventory.Managment.Core/IUnitOfWork.cs
@@ -1,13 +1,11 @@
 ﻿using Inventory.Managment.Core.Entities;
 using Inventory.Managment.Core.Repositories.Contracts;
-using System.Collections;
 
 namespace Inventory.Managment.Core
 {
     public interface IUnitOfWork : IAsyncDisposable
     {
-        Hashtable _repositories { get; set; }
-        IGenericRepository<T> Repository<T>() where T : BaseEntity;
+        IGenericRepository<T>? Repository<T>() where T : BaseEntity;
 
         Task<int> CompleteAsync();
     }
